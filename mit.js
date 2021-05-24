3b02b9e9577ed728635ed7f787954967e157d562
Samira Liebendörfer <samira@prolog.work>
2021-05-24 22:18:07 +0200

    fixes colors, date, motto

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
-    city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+  city: "Cambridge",
+  colors: "Silver Gray",
+  mascot: "robot",
+  founded: 1869,
+  motto: "Mens et Manus",
 };
